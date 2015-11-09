9384b5889719e16b61f2123a6f5ee91c37820a8f
Timoshenko <carlostimoshenkorodrigueslopes@gmail.com>
2015-11-09 08:53:24 -0200

    still on development
    =D

---
+++ setup-insetall.sh
@@ -0,0 +1,11 @@
+# Author: carlostimoshenkorodrigueslopes@gmail.com
+
+sudo yum install check pam pam-devel freetype libpng libjpeg zlib luajit luajit-devel dbus dbus-devel \
+libXcursor libXcursor-devel libXrender libXrender-devel libXrandr libXrandr-devel \
+libXfixes libXfixes-devel libxdamage libxdamage-devel libXcomposite libXcomposite-devel \
+libXScrnSaver libXScrnSaver-devel libXp libXp-devel libXext libXext-devel libXinerama libXinerama-devel \
+libxkbfile libxkbfile-devel libXtst libXtst-devel libxcb libxcb-devel xcb* pulseaudio-libs pulseaudio-libs-devel \
+libsndfile libsndfile-devel systemd systemd-devel libblkid libblkid-devel libmount libmount-devel gstreamer1 gstreamer1-devel \
+libtiff libtiff-devel giflib giflib-devel mesa-libGL mesa-libGL-devel libspectre libspectre-devel poppler poppler-devel \
+librsvg2 librsvg2-devel LibRaw LibRaw-devel xine-lib xine-lib-devel bullet bullet-devel libwebp libwebp-devel fribidi fribidi-devel
+
